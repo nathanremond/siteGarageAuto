92c6035ecc4628d2879ab4811e906f357fca5a47
nathanremond <perso.nathan.remond@gmail.com>
2024-09-24 18:21:30 +0200

    Création entité car

---
+++ migrations/Version20240924161949.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240924161949 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE car (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT NOT NULL, technical_infos LONGTEXT NOT NULL, price DOUBLE PRECISION NOT NULL, first_photo VARCHAR(255) NOT NULL, circulation_year DATE NOT NULL, mileage DOUBLE PRECISION NOT NULL, equipment LONGTEXT NOT NULL, options LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE car');
+    }
+}
---
+++ src/Entity/Car.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarRepository::class)
+ */
+class Car
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $technicalInfos;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstPhoto;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $circulationYear;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $mileage;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $equipment;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $options;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTechnicalInfos(): ?string
+    {
+        return $this->technicalInfos;
+    }
+
+    public function setTechnicalInfos(string $technicalInfos): self
+    {
+        $this->technicalInfos = $technicalInfos;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getFirstPhoto(): ?string
+    {
+        return $this->firstPhoto;
+    }
+
+    public function setFirstPhoto(string $firstPhoto): self
+    {
+        $this->firstPhoto = $firstPhoto;
+
+        return $this;
+    }
+
+    public function getCirculationYear(): ?\DateTimeInterface
+    {
+        return $this->circulationYear;
+    }
+
+    public function setCirculationYear(\DateTimeInterface $circulationYear): self
+    {
+        $this->circulationYear = $circulationYear;
+
+        return $this;
+    }
+
+    public function getMileage(): ?float
+    {
+        return $this->mileage;
+    }
+
+    public function setMileage(float $mileage): self
+    {
+        $this->mileage = $mileage;
+
+        return $this;
+    }
+
+    public function getEquipment(): ?string
+    {
+        return $this->equipment;
+    }
+
+    public function setEquipment(string $equipment): self
+    {
+        $this->equipment = $equipment;
+
+        return $this;
+    }
+
+    public function getOptions(): ?string
+    {
+        return $this->options;
+    }
+
+    public function setOptions(string $options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
