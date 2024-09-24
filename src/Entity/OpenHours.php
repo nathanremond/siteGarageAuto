e1ef58f29e7682c4361bf79f078a8bf20bb323b5
nathanremond <perso.nathan.remond@gmail.com>
2024-09-24 18:10:43 +0200

    Création entité openHours

---
+++ src/Entity/OpenHours.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OpenHoursRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OpenHoursRepository::class)
+ */
+class OpenHours
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $day;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $openTime;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $closeTime;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $state;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?string
+    {
+        return $this->day;
+    }
+
+    public function setDay(string $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getOpenTime(): ?\DateTimeInterface
+    {
+        return $this->openTime;
+    }
+
+    public function setOpenTime(\DateTimeInterface $openTime): self
+    {
+        $this->openTime = $openTime;
+
+        return $this;
+    }
+
+    public function getCloseTime(): ?\DateTimeInterface
+    {
+        return $this->closeTime;
+    }
+
+    public function setCloseTime(\DateTimeInterface $closeTime): self
+    {
+        $this->closeTime = $closeTime;
+
+        return $this;
+    }
+
+    public function isState(): ?bool
+    {
+        return $this->state;
+    }
+
+    public function setState(bool $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
