b7ca4cc70cc5dd15ebcc27961f17262f05b39091
nathanremond <perso.nathan.remond@gmail.com>
2024-09-27 10:37:03 +0200

    CRUD Notice

---
+++ src/Form/NoticeType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Notice;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NoticeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('comment')
+            ->add('note')
+            ->add('state')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Notice::class,
+        ]);
+    }
+}
