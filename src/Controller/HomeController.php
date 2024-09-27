9439b67af7d972f93f94175ccd15f423d5959677
nathanremond <perso.nathan.remond@gmail.com>
2024-09-27 15:22:08 +0200

    Gestion des avis + finition page espace dédié

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Notice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use App\Repository\ServiceRepository;
 use App\Repository\CarRepository;
 use App\Repository\NoticeRepository;
 use App\Repository\OpenHoursRepository;
-
+use Symfony\Component\HttpFoundation\Request;
 
 
 class HomeController extends AbstractController
@@ -17,14 +18,41 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(ServiceRepository $ServiceRepository, CarRepository $CarRepository, NoticeRepository $NoticeRepository, OpenHoursRepository $OpenHoursRepository): Response
+    public function index(ServiceRepository $serviceRepository, CarRepository $carRepository, NoticeRepository $noticeRepository, OpenHoursRepository $openHoursRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'services' => $ServiceRepository->findAll(),
-            'cars' => $CarRepository->findAll(),
-            'notices' => $NoticeRepository->findAll(),
-            'openHours' => $OpenHoursRepository->findAll()
+            'services' => $serviceRepository->findAll(),
+            'cars' => $carRepository->findAll(),
+            'notices' => $noticeRepository->findby(
+                ['state'=>'validated']
+            ),
+            'openHours' => $openHoursRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/createNotice", name="app_create_notice",  methods={"POST"})
+     */
+    public function createNotice(Request $request, ServiceRepository $serviceRepository, CarRepository $carRepository, NoticeRepository $noticeRepository, OpenHoursRepository $openHoursRepository): Response
+    {
+        $notice = new Notice();
+
+        $notice->setName($request->get('name'));
+        $notice->setComment($request->get('comment'));
+        $notice->setNote($request->get('note'));
+        $notice->setState('new');
+
+        $noticeRepository->add($notice, true);
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'services' => $serviceRepository->findAll(),
+            'cars' => $carRepository->findAll(),
+            'notices' => $noticeRepository->findby(
+                ['state'=>'validated']
+            ),
+            'openHours' => $openHoursRepository->findAll()
         ]);
     }
 }
